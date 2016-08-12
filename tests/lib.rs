1dfcc6e12d1b8813996eeff42dfb9e2d09a46f92
Takeru Ohta <phjgt308@gmail.com>
2016-08-12 15:01:43 +0900

    Add test cases for SplayMap

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,25 +5,155 @@ mod map {
     use splay_tree::SplayMap;
 
     #[test]
-    fn basic() {
+    fn new() {
+        let map: SplayMap<(), ()> = SplayMap::new();
+        assert!(map.is_empty());
+    }
+
+    #[test]
+    fn default() {
+        let map: SplayMap<(), ()> = SplayMap::default();
+        assert!(map.is_empty());
+    }
+
+    #[test]
+    fn insert_and_contains_key() {
         let mut map = SplayMap::new();
-        assert_eq!(0, map.len());
-        assert_eq!(None, map.get("foo"));
-        assert_eq!(None, map.insert("foo", 1));
-        assert_eq!(Some(&1), map.get("foo"));
-        assert_eq!(1, map.len());
-        assert_eq!(Some(1), map.remove("foo"));
-        assert_eq!(None, map.get("foo"));
-        assert_eq!(0, map.len());
+        assert!(!map.contains_key("foo"));
+        assert_eq!(map.insert("foo", 1), None);
+        assert!(map.contains_key("foo"));
+        assert_eq!(map.insert("foo", 2), Some(1));
+        assert!(map.contains_key("foo"));
+    }
 
+    #[test]
+    fn clear() {
+        let mut map = SplayMap::new();
+        map.insert("foo", 1);
+        assert!(map.contains_key("foo"));
         map.clear();
-        assert_eq!(None, map.get("foo"));
+        assert!(!map.contains_key("foo"));
+    }
 
+    #[test]
+    fn get_and_get_mut() {
+        let mut map = SplayMap::new();
+        assert_eq!(map.get("foo"), None);
+        map.insert("foo", 1);
+        assert_eq!(map.get("foo"), Some(&1));
+
+        *map.get_mut("foo").unwrap() += 10;
+        assert_eq!(map.get("foo"), Some(&11));
+    }
+
+    #[test]
+    fn iterator() {
+        let mut map: SplayMap<_, _> =
+            vec![("foo", 1), ("bar", 2), ("baz", 3)].into_iter().collect();
+        assert_eq!(vec!["bar", "baz", "foo"],
+                   map.keys().cloned().collect::<Vec<_>>());
+        assert_eq!(vec![2, 3, 1], map.values().cloned().collect::<Vec<_>>());
+        for v in map.values_mut() {
+            *v += 10;
+        }
+        assert_eq!(vec![("bar", 12), ("baz", 13), ("foo", 11)],
+                   map.into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn find_lower_or_upper_bound() {
+        // small map
+        let mut map: SplayMap<_, _> =
+            vec![("foo", 1), ("bar", 2), ("baz", 3)].into_iter().collect();
+        assert_eq!(map.find_lower_bound("aaa"), Some(&"bar"));
+        assert_eq!(map.find_upper_bound("aaa"), Some(&"bar"));
+        assert_eq!(map.find_lower_bound("baz"), Some(&"baz"));
+        assert_eq!(map.find_upper_bound("baz"), Some(&"foo"));
+        assert_eq!(map.find_lower_bound("zzz"), None);
+        assert_eq!(map.find_upper_bound("zzz"), None);
+
+        // large map
+        use rand::{self, Rng};
+        let mut input = (0..1000).into_iter().collect::<Vec<_>>();
+        rand::thread_rng().shuffle(&mut input);
+
+        let mut map: SplayMap<_, _> = input.into_iter().map(|n| (n, n)).collect();
+        assert_eq!(map.find_lower_bound(&500), Some(&500));
+        assert_eq!(map.find_upper_bound(&500), Some(&501));
+        assert_eq!(map.find_lower_bound(&999), Some(&999));
+        assert_eq!(map.find_upper_bound(&999), None);
+    }
+
+    #[test]
+    fn remove() {
+        let mut map = SplayMap::new();
         map.insert("foo", 1);
         map.insert("bar", 2);
         map.insert("baz", 3);
-        assert_eq!(vec![("bar", 2), ("baz", 3), ("foo", 1)],
-                   map.iter().map(|(k, v)| (*k, *v)).collect::<Vec<_>>());
+        assert_eq!(map.remove("bar"), Some(2));
+        assert_eq!(map.remove("bar"), None);
+        assert_eq!(map.len(), 2);
+    }
+
+    #[test]
+    fn enry() {
+        let mut count = SplayMap::new();
+
+        for x in vec!["a", "b", "a", "c", "a", "b"] {
+            *count.entry(x).or_insert(0) += 1;
+        }
+
+        assert_eq!(count.get("a"), Some(&3));
+        assert_eq!(count.get("b"), Some(&2));
+        assert_eq!(count.get("c"), Some(&1));
+    }
+
+    #[test]
+    fn extend() {
+        let mut map = SplayMap::new();
+        map.extend(vec![("foo", 1), ("bar", 2)]);
+        map.extend(vec![("bar", 3), ("baz", 4)]);
+        assert_eq!(vec![("bar", 3), ("baz", 4), ("foo", 1)],
+                   map.into_iter().collect::<Vec<_>>());
+    }
+
+    #[test]
+    fn hash_eq_ord() {
+        let mut a: SplayMap<_, _> = vec![("bar", 2), ("baz", 3)].into_iter().collect();
+        let mut b: SplayMap<_, _> = vec![("foo", 1), ("bar", 2)].into_iter().collect();
+
+        assert!(hash(&a) != hash(&b));
+        assert!(a != b);
+        assert!(a < b);
+
+        b.insert("baz", 3);
+        assert!(a < b);
+
+        a.insert("foo", 1);
+        assert!(hash(&a) == hash(&b));
+        assert!(a == b);
+    }
+
+    #[test]
+    fn large_map() {
+        use rand::{self, Rng};
+
+        let mut input = (0..1000).into_iter().collect::<Vec<_>>();
+        rand::thread_rng().shuffle(&mut input);
+
+        let mut map: SplayMap<_, _> = input.into_iter().map(|n| (n, n)).collect();
+        for i in 0..1000 {
+            assert_eq!(map.remove(&i), Some(i));
+        }
+        assert!(map.is_empty());
+    }
+
+    use std::hash;
+    fn hash<T: hash::Hash>(x: &T) -> u64 {
+        use std::hash::Hasher;
+        let mut hasher = hash::SipHasher::new();
+        x.hash(&mut hasher);
+        hasher.finish()
     }
 }
 
